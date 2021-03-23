23ee5ce2e9e7a62a7ac0dfdbe95a5c7e6aab4317
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-23 11:38:02 -0700

    add fadeToggle feature to scripts.js
    Co-authored-by: Patrick Lee <patricklee1138@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
+  });
+})
+
+
+// coding for fades:
     // $('#walrus-showing').fadeIn('slow');
-    $("#walrus-hidden").toggle();
     // $('#walrus-hidden').fadeIn('slow');
     // See top two answers here: https://stackoverflow.com/questions/2625887/jquery-toggle-and-fade-in-one-function
-  });
-});
