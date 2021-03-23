adbd3c462947b01b5181b5a0f2f395af9503c119
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-03-23 11:08:50 -0700

    add show/hide js feature to walrus.jpg

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    // $('#walrus-showing').fadeIn('slow');
+    $("#walrus-hidden").toggle();
+    // $('#walrus-hidden').fadeIn('slow');
+    // See top two answers here: https://stackoverflow.com/questions/2625887/jquery-toggle-and-fade-in-one-function
   });
 });
